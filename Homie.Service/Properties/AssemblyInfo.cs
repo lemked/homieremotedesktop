5497ec42bfa2267431f6041363b27baf9af4001a
Daniel Lemke <lemked@web.de>
2015-02-03 21:17:31 +0100

    Modified description and version number

--- Homie.Service/Properties/AssemblyInfo.cs
+++ Homie.Service/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HomieService Service")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The smart WOL/RDP tool.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HomieService Remotedesktop")]
-[assembly: AssemblyCopyright("Copyright © 2014 Daniel Lemke")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Daniel Lemke")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
