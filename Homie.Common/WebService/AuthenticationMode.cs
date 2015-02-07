48eead308ef064d10b5ed7c4513696dece4a57dd
Daniel Lemke <lemked@web.de>
2015-02-07 17:42:10 +0100

    Renamed enum SecurityMode to AuthenticationMode

---
+++ Homie.Common/WebService/AuthenticationMode.cs
@@ -0,0 +1,28 @@
+﻿namespace Homie.Common.WebService
+{
+    /// <summary>
+    /// Defines the available authentication modes.
+    /// </summary>
+    public enum AuthenticationMode
+    {
+        /// <summary>
+        /// No authentication.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Authentication via credentials (username / password).
+        /// </summary>
+        Credentials,
+
+        /// <summary>
+        /// Certificate based authentication.
+        /// </summary>
+        Certificate,
+
+        /// <summary>
+        /// Certificate based authentication with credentials.
+        /// </summary>
+        CertificateAndCredentials
+    }
+}
