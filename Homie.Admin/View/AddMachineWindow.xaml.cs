e074638a89c8db255ee8f20ae0ee6f63390f873d
Daniel Lemke <lemked@web.de>
2015-02-09 20:29:36 +0100

    Removed obsolete code

--- Homie.Admin/View/AddMachineWindow.xaml.cs
+++ Homie.Admin/View/AddMachineWindow.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Globalization;
-using System.ServiceModel;
-using System.Windows;
-using Homie.Model;
-
-namespace Homie.Admin.View
+﻿namespace Homie.Admin.View
 {
     /// <summary>
     /// Interaction logic for AddMachineWindow.xaml
     /// </summary>
-    public partial class AddMachineWindow : Window
+    public partial class AddMachineWindow
     {
-        private Machine m_CurrentMachine;
-
         public AddMachineWindow()
         {
             InitializeComponent();
