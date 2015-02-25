5cabb0aa2e80f9b801470b3be89a2b342c19528a
Daniel Lemke <lemked@web.de>
2015-02-25 20:29:54 +0100

    Updated WebServiceFactory to support secure connections

--- Homie.Common/WebService/WebServiceFactory.cs
+++ Homie.Common/WebService/WebServiceFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
-using System.ServiceModel.Security;
 using Homie.Common.Interfaces;
 
 namespace Homie.Common.WebService
@@ -11,32 +10,42 @@ namespace Homie.Common.WebService
     /// </summary>
     public class WebServiceFactory
     {
-        private Binding binding;
-        private string address;
-        private int port;
-        private string serviceEndPoint;
+        public Binding Binding { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public string ServiceEndPoint { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
         public WebServiceFactory(Binding binding, string address, int port, string serviceEndPoint)
         {
-            this.binding = binding;
-            this.address = address;
-            this.port = port;
-            this.serviceEndPoint = serviceEndPoint;
+            this.Binding = binding;
+            this.Address = address;
+            this.Port = port;
+            this.ServiceEndPoint = serviceEndPoint;
         }
 
-        public T Create<T>(UserNamePasswordClientCredential credentials = null)
+        public WebServiceFactory(Binding binding, string address, int port, string serviceEndPoint, string username, string password)
+            : this(binding, address, port, serviceEndPoint)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public T Create<T>()
         {
             // Determine the protocol of the web service
             var protocol = Protocol.Http; // default
-            if (binding is BasicHttpsBinding)
+            if (Binding is BasicHttpsBinding)
             {
                 protocol = Protocol.Https;
             }
-            else if (binding is NetTcpBinding)
+            else if (Binding is NetTcpBinding)
             {
                 protocol = Protocol.NetTcp;
             }
 
-            var endPointUrl = String.Format(Constants.WebServiceUrlTemplate, protocol, address, port, serviceEndPoint);
+            var endPointUrl = String.Format(Constants.WebServiceUrlTemplate, protocol, Address, Port, ServiceEndPoint);
 
             if (typeof(T) == typeof(IMachineControlService))
             {
@@ -53,12 +62,12 @@ namespace Homie.Common.WebService
 
             var lAddress = new EndpointAddress(new Uri(endPointUrl));
 
-            var lFactory = new ChannelFactory<T>(binding, lAddress);
+            var lFactory = new ChannelFactory<T>(Binding, lAddress);
 
-            if (lFactory.Credentials != null && credentials != null)
+            if (lFactory.Credentials != null && !string.IsNullOrEmpty(this.Username) && !string.IsNullOrEmpty(Password))
             {
-                lFactory.Credentials.UserName.UserName = credentials.UserName;
-                lFactory.Credentials.UserName.Password = credentials.Password;
+                lFactory.Credentials.UserName.UserName = Username;
+                lFactory.Credentials.UserName.Password = Password;
             }
 
             return lFactory.CreateChannel();
