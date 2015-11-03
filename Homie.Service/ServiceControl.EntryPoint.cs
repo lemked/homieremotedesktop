6e8d58c7f216e2502ea45e7013f2e62b78d9df78
Daniel Lemke <lemked@web.de>
2015-11-03 21:35:30 +0100

    Move Main method related methods in separate partial class.

---
+++ Homie.Service/ServiceControl.EntryPoint.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+using Homie.Common;
+using Homie.Common.Interfaces;
+using Homie.Common.Logging;
+using Homie.Model;
+using Homie.Model.Logging;
+using Homie.Service.Settings;
+
+namespace Homie.Service
+{
+    public partial class ServiceControl : ServiceBase
+    {
+        /// <summary>
+        /// The starting point of the service application.
+        /// </summary>
+        /// <param name="arguments">The p arguments.</param>
+        public static void Main(string[] arguments)
+        {
+            // Register global event handlers for unhandled exceptions.
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
+
+            // Set up depedency injection.
+            DependencyInjector.Register<IServiceLogDataSource, DbServiceLogDataSource>();
+            DependencyInjector.Register<IServiceLogProvider, ServiceLogProvider>();
+            DependencyInjector.Register<IMachineControlService, MachineControlService>();
+            DependencyInjector.Register<IUserControlService, UserControlService>();
+            DependencyInjector.Register<IServiceSettingsProvider, DbServiceSettingsProvider>();
+            DependencyInjector.Register<IMachineDataSource, DbMachineDataSource>();
+            DependencyInjector.Register<IUserDataSource, DbUserDataSource>();
+
+            // Configure default logger
+            ILogger textLogger = new FileLogger();
+            textLogger.LogLevel = LogLevel.Trace;
+            Log.Register(textLogger);
+
+            foreach (string lArgument in arguments)
+            {
+                if (lArgument.Equals(NoServiceArgument, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    RunInConsoleMode();
+                    return;
+                }
+
+                if (lArgument.Equals(InstallServiceArgument, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                    return;
+                }
+            }
+
+            Run(new ServiceControl()); // TODO: Use DI container
+        }
+
+        private static void TaskSchedulerUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Log.Exception(e.Exception);
+        }
+
+        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception lException = ExceptionUtils.UnwrapExceptionObject(e.ExceptionObject);
+            Log.Exception(lException);
+            Environment.Exit((int)ExceptionUtils.ExitCodes.UnhandledException);
+        }
+
+        private static void RunInConsoleMode()
+        {
+            // Configure console logger
+            ILogger consoleLogger = new ConsoleLogger();
+            consoleLogger.LogLevel = LogLevel.Debug;
+            Log.Register(consoleLogger);
+
+            Log.Info(Resources.Properties.Resources.ServiceStartedWith, NoServiceArgument);
+            var lServiceControl = new ServiceControl();
+            lServiceControl.OnStart(new string[0]);
+
+            Console.WriteLine(@"Service started, press any key to finish execution.");
+            Console.ReadKey();
+            lServiceControl.OnStop();
+        }
+    }
+}
--- Homie.Service/ServiceControl.cs
+++ Homie.Service/ServiceControl.cs
@@ -2,26 +2,22 @@
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.IO;
-using System.Reflection;
 using System.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Security;
 using System.ServiceProcess;
-using System.Threading.Tasks;
 using Homie.Common;
 using Homie.Common.Interfaces;
 using Homie.Common.Logging;
-using Homie.Common.WebService;
 using Homie.Model;
 using Homie.Model.Logging;
-using Homie.Service.Properties;
 using Homie.Service.Settings;
 
 namespace Homie.Service
 {
-    public class ServiceControl : ServiceBase
+    public partial class ServiceControl : ServiceBase
     {
         private const string NoServiceArgument = "/NOSERVICE";
         private const string InstallServiceArgument = "/INSTALL";
@@ -45,76 +41,9 @@ namespace Homie.Service
         }
 
         /// <summary>
-        /// The starting point of the service application.
+        /// Called when the service is started.
         /// </summary>
-        /// <param name="arguments">The p arguments.</param>
-        public static void Main(string[] arguments)
-        {
-            // Register global event handlers for unhandled exceptions.
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
-            TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
-
-            // Set up depedency injection.
-            DependencyInjector.Register<IServiceLogDataSource, DbServiceLogDataSource>();
-            DependencyInjector.Register<IServiceLogProvider, ServiceLogProvider>();
-            DependencyInjector.Register<IMachineControlService, MachineControlService>();
-            DependencyInjector.Register<IUserControlService, UserControlService>();
-            DependencyInjector.Register<IServiceSettingsProvider, DbServiceSettingsProvider>();
-            DependencyInjector.Register<IMachineDataSource, DbMachineDataSource>();
-            DependencyInjector.Register<IUserDataSource, DbUserDataSource>();
-
-            // Configure default logger
-            ILogger textLogger = new FileLogger();
-            textLogger.LogLevel = LogLevel.Trace;
-            Log.Register(textLogger);
-            
-            foreach (string lArgument in arguments)
-            {
-                if (lArgument.Equals(NoServiceArgument, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    RunInConsoleMode();
-                    return;
-                }
-
-                if (lArgument.Equals(InstallServiceArgument, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
-                    return;
-                }
-            }
-
-            Run(new ServiceControl()); // TODO: Use DI container
-        }
-
-        private static void TaskSchedulerUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-        {
-            Log.Exception(e.Exception);
-        }
-
-        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception lException = ExceptionUtils.UnwrapExceptionObject(e.ExceptionObject);
-            Log.Exception(lException);
-            Environment.Exit((int) ExceptionUtils.ExitCodes.UnhandledException);
-        }
-
-        private static void RunInConsoleMode()
-        {
-            // Configure console logger
-            ILogger consoleLogger = new ConsoleLogger();
-            consoleLogger.LogLevel = LogLevel.Debug;
-            Log.Register(consoleLogger);
-
-            Log.Info(Resources.Properties.Resources.ServiceStartedWith, NoServiceArgument);
-            var lServiceControl = new ServiceControl();
-            lServiceControl.OnStart(new string[0]);
-
-            Console.WriteLine(@"Service started, press any key to finish execution.");
-            Console.ReadKey();
-            lServiceControl.OnStop();
-        }
-
-        // Start the Windows service.
+        /// <param name="arguments">The arguments.</param>
         protected override void OnStart(string[] arguments)
         {
             Log.Info(Resources.Properties.Resources.ServiceIsStarting);
